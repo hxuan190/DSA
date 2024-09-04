e65791996efe7032a68ca3cb8ebe4ed5446d12f6
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-09-04 22:10:25 +0700

    up

--- Generics/main.go
+++ Generics/main.go
@@ -2,34 +2,32 @@ package main
 
 import "fmt"
 
-// Hàm sử dụng generics để hoạt động với bất kỳ kiểu dữ liệu nào
-func Print[T any](items []T) {
-	for _, item := range items {
-		fmt.Println(item)
-	}
+// Định nghĩa kiểu dữ liệu Stack với type parameter T
+type Stack[T any] struct {
+	elements []T
 }
 
-// Định nghĩa một interface chỉ cho phép các kiểu dữ liệu có thể so sánh được
-type Comparable interface {
-	int | float64 | string
+// Phương thức Push thêm phần tử vào Stack
+func (s *Stack[T]) Push(v T) {
+	s.elements = append(s.elements, v)
 }
 
-// Hàm sử dụng generics với ràng buộc kiểu Comparable
-func Max[T Comparable](a, b T) T {
-	if a > b {
-		return a
-	}
-	return b
+// Phương thức Pop lấy phần tử từ Stack
+func (s *Stack[T]) Pop() T {
+	lastIndex := len(s.elements) - 1
+	element := s.elements[lastIndex]
+	s.elements = s.elements[:lastIndex]
+	return element
 }
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5}
-	strs := []string{"Go", "is", "awesome"}
+	intStack := Stack[int]{}
+	intStack.Push(10)
+	intStack.Push(20)
+	fmt.Println(intStack.Pop()) // Output: 20
 
-	Print(nums)
-	Print(strs)
-
-	fmt.Println(Max(3, 4))         // Output: 4
-	fmt.Println(Max(3.5, 2.1))     // Output: 3.5
-	fmt.Println(Max("Go", "Rust")) // Output: Rust
+	stringStack := Stack[string]{}
+	stringStack.Push("Hello")
+	stringStack.Push("World")
+	fmt.Println(stringStack.Pop()) // Output: World
 }
